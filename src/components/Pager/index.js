99c418908c52e69b6ad580f68c4253a265165024
Charles Howard <frodare@gmail.com>
2018-01-18 21:23:54 -0500

    pager work

--- src/components/Pager/index.js
+++ src/components/Pager/index.js
@@ -16,40 +16,31 @@ const FirstButton = ({currentPage, changePage}) => {
   )
 }
 
-const renderPrevious = (currentPage, handlePageChangeProp) => {
-  if (currentPage > 1) {
-    return (
-      <div
-        className='btn btn-default pager-button previous'
-        onClick={() => handlePageChange(currentPage - 1, handlePageChangeProp)}>
-        {'Previous'}
-      </div>
-    )
-  }
+const PreviousButton = ({currentPage, changePage}) => {
+  if (currentPage === 0) return null
+  return (
+    <div data-page={currentPage - 1} className='btn btn-default pager-button previous' onClick={changePage}>
+      {'Previous'}
+    </div>
+  )
 }
 
-const renderNext = (currentPage, totalPages, handlePageChangeProp) => {
-  if (currentPage < totalPages) {
-    return (
-      <div
-        className='btn btn-default pager-button next'
-        onClick={() => handlePageChange(currentPage + 1, handlePageChangeProp)}>
-        {'Next'}
-      </div>
-    )
-  }
+const NextButton = ({currentPage, totalPages, changePage}) => {
+  if (currentPage === totalPages - 1) return null
+  return (
+    <div data-page={currentPage + 1} className='btn btn-default pager-button next' onClick={changePage}>
+      {'Next'}
+    </div>
+  )
 }
 
-const renderLast = (currentPage, totalPages, handlePageChangeProp) => {
-  if (currentPage < totalPages - 1) {
-    return (
-      <div
-        className='btn btn-default pager-button last'
-        onClick={() => handlePageChange(totalPages, handlePageChangeProp)}>
-        {'Last'}
-      </div>
-    )
-  }
+const LastButton = ({currentPage, totalPages, changePage}) => {
+  if (currentPage === totalPages - 1) return null
+  return (
+    <div data-page={totalPages - 1} className='btn btn-default pager-button last' onClick={changePage}>
+      {'Last'}
+    </div>
+  )
 }
 
 // TODO handle remainder correctly
@@ -58,31 +49,8 @@ const determineTotalPages = (pageSize, count) => count / pageSize
 const determinePages = (currentPage, count) => {
   const pages = []
   const totalPages = determineTotalPages(currentPage, count)
-
   for (let i = currentPage - 2; i <= currentPage + 2; i++) {
-    if (i > 0 && i <= totalPages) pages.push(i)
-  }
-
-  return pages
-}
-
-const renderPages = (currentPage, totalPages, handlePageChangeProp) => {
-  const pages = []
-  for (let i = currentPage - 2; i <= currentPage + 2; i++) {
-    if (i > 0 && i <= totalPages) {
-      if (i === currentPage) {
-        pages.push(<div key={i} className='pager-button btn btn-primary current disabled'>{i}</div>)
-      } else {
-        pages.push(
-          <div
-            key={i}
-            className='btn pager-button btn btn-default'
-            onClick={() => handlePageChange(i, handlePageChangeProp)}>
-            {i}
-          </div>
-        )
-      }
-    }
+    if (i > 0 && i <= totalPages) pages.push(i - 1)
   }
 
   return pages
@@ -90,7 +58,7 @@ const renderPages = (currentPage, totalPages, handlePageChangeProp) => {
 
 const PageButton = ({page, currentPage, changePage}) => (
   <div data-page={page} className='btn pager-button btn btn-default' onClick={changePage}>
-    {page}
+    {page + 1}
   </div>
 )
 
@@ -99,15 +67,17 @@ const Pager = ({changePage, filter, count}) => {
 
   const totalPages = 300
 
-  const currentPage = 3
+  const currentPage = 30
 
   return (
     <div className='manifest-pager'>
       <FirstButton changePage={changePage} currentPage={currentPage} />
-      {renderPrevious(currentPage, handlePageChange)}
-      {renderPages(currentPage, totalPages, handlePageChange)}
-      {renderNext(currentPage, totalPages, handlePageChange)}
-      {renderLast(currentPage, totalPages, handlePageChange)}
+      <PreviousButton changePage={changePage} currentPage={currentPage} />
+      {determinePages(currentPage, count).map((page, index) => (
+        <PageButton key={index} page={page} currentPage={currentPage} changePage={changePage} />
+      ))}
+      <NextButton changePage={changePage} currentPage={currentPage} totalPages={totalPages} />
+      <LastButton changePage={changePage} currentPage={currentPage} totalPages={totalPages} />      
     </div>
   )
 }
