687729c247b55445ab04c64dbaf75f8251b977bd
Alexander <Alemkin@users.noreply.github.com>
2018-01-23 13:41:08 -0500

    updated package json for npm, and added index to export required files (#1)
    * updated package json for npm, and added index to export required files
    
    * Made requested changes
    
    * Switching to imports
    
    * Added new line

---
+++ index.js
@@ -0,0 +1,13 @@
+import Headers from './src/containers/Headers'
+import Manifest from './src/containers/Manifest'
+import Pager from './src/containers/Pager'
+import PageSizer from './src/containers/PageSizer'
+import Rows from './src/containers/Rows'
+import Status from './src/containers/Status'
+
+export const Headers
+export const Manifest
+export const Pager
+export const PageSizer
+export const Rows
+export const Status
