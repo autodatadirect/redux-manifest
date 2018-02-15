e2751529a8ef3d267ea203f6428dcbf06b37acd7
Alexander Lemkin <alemkin@add123.com>
2018-02-15 14:34:19 -0500

    #13 #11 #10 made requested changes

--- src/containers/PageSizer.js
+++ src/containers/PageSizer.js
@@ -5,7 +5,6 @@ import { compose, withHandlers } from 'recompose'
 import PageSizer from '../components/PageSizer'
 import * as actions from '../actions'
 import stateByName from '../util/stateByName'
-import { initialState } from '../reducer'
 
 const mapStateToProps = (state, props) => {
   const namedState = stateByName(state, props.name)
@@ -22,7 +21,7 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 const handlers = {
   changePageSize: props => event => {
     const pageSize = event.target.value
-    props.refreshData(props.name, {...initialState.filter, pageSize: pageSize})
+    props.refreshData(props.name, {...props.filter, page: 0, pageSize: pageSize})
   }
 }
 
