93e07e80aefbe7c3aa45b92abfb4c9396c2b8b51
Charles Howard <frodare@gmail.com>
2018-03-08 07:40:12 -0500

    got store mostly working and added some React imports

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from '../src/reducer'
+import { createStore, combineReducers } from 'redux'
+import manifestReducer from '../src/reducer'
 import * as actions from '../src/actions'
 
-const store = createStore(reducer)
-store.dispatch(actions.refreshData('testmanifest', {page: 0, pageSize: 10}))
+const reducer = combineReducers({
+  manifest: manifestReducer
+})
 
-console.log('state', store.getState())
+const data = [{
+  id: 1,
+  col1: 'row1a',
+  col2: 'row1b'
+},{
+  id: 2,
+  col1: 'row2'
+},{
+  id: 3,
+  col1: 'row3'
+}]
+
+const store = createStore(reducer)
+store.dispatch(actions.setPage('testmanifest', data, 12312))
 
 export const withRedux = story => <Provider store={store}>{story()}</Provider>
