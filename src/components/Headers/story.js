6d194c473556ac8ca3e865abc1011a30b6687d97
Sergio Rodriguez <oneandprime@gmail.com>
2018-08-04 08:30:35 -0400

    Added headerComponent handling to override basic header (#46)

--- src/components/Headers/story.js
+++ src/components/Headers/story.js
@@ -5,6 +5,12 @@ import { withKnobs, text, boolean, object } from '@storybook/addon-knobs/react'
 import { action } from '@storybook/addon-actions'
 import { withRedux } from '../../../.storybook/decorators'
 
+const CustomHeader = ({ label }) =>
+  <th>
+    <div>THIS IS A TEST.</div>
+    <div>{label}</div>
+  </th>
+
 const stories = storiesOf('Headers', module)
 stories.addDecorator(withKnobs)
 stories.addDecorator(withRedux)
@@ -33,3 +39,25 @@ stories.add('Basic', () => (
     }])}
   />
 ))
+stories.add('Custom', () => {
+  const def = [{
+    id: 'col1',
+    label: 'Customer Header 1',
+    headerComponent: CustomHeader
+  }, {
+    id: 'col2',
+    label: 'Basic Header 2'
+  }]
+
+  return (<Headers
+    name={text('name', 'testmanifest')}
+    loading={boolean('loading', false)}
+
+    updateSort={action('updateSort')}
+    sorts={object('sorts', [{
+      id: 'test',
+      isAsc: true
+    }])}
+    definition={def}
+  />)
+})
