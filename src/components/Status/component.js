83b5a6e5c8868e394267f2cb368004b39d593952
Zane Riley <zeriley@gmail.com>
2018-12-21 15:44:16 -0500

    Pager fix (#69)
    * 1.0.19
    
    * fixing paging issues

--- src/components/Status/component.js
+++ src/components/Status/component.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const determineFirstOnPage = (page, pageSize) => Math.max(page * pageSize, 1)
+const determineFirstOnPage = (page, pageSize) => Math.max(page * pageSize, 0)
 
 const determineLastOnPage = (page, pageSize, count) => Math.min(determineFirstOnPage(page + 1, pageSize), count)
 
@@ -9,7 +9,7 @@ const renderTotalRecordsMessage = (loadingCount, count, filter, error) => {
   if (loadingCount) return 'loading...'
   if (error) return error
   const last = determineLastOnPage(filter.page, filter.pageSize, count)
-  const first = determineFirstOnPage(filter.page, filter.pageSize)
+  const first = determineFirstOnPage(filter.page, filter.pageSize) + 1
   return count < 1 ? 'No Results' : `Showing ${first} to ${last} of ${count}`
 }
 
